a409d2ab6c2206cfe037de24c997b96d730ea34f
jccjd <1340944538@qq.com>
2018-06-02 15:31:54 +0800

    update

--- app/src/main/java/com/example/lenovo/rx_project/android_rx/RxUtils.java
+++ app/src/main/java/com/example/lenovo/rx_project/android_rx/RxUtils.java
@@ -1,5 +1,7 @@
 package com.example.lenovo.rx_project.android_rx;
 
+import android.util.Log;
+
 import rx.Observable;
 import rx.Subscriber;
 
@@ -13,9 +15,33 @@ public class RxUtils {
             public void call(Subscriber<? super String> subscriber) {
                 if (!subscriber.isUnsubscribed()) {
                     subscriber.onNext("hello");
-
+                    subscriber.onNext("hi");
+                    subscriber.onNext(downLoadJson());
+                    subscriber.onNext("World");
+                    subscriber.onCompleted();
                 }
             }
         });
+        Subscriber showsub = new Subscriber() {
+            @Override
+            public void onCompleted() {
+                Log.i(TAG,"onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG,e.getMessage());
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.i(TAG,"result->>"+o);
+            }
+        };
+        observable.subscribe(showsub);//关联被观察者
+
+    }
+    public static String downLoadJson() {
+        return "json data";
     }
 }
